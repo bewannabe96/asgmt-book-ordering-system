43345f639dc48c0e4326ec616d855726b9b96390
Andrew <jhc5829@gmail.com>
2021-04-06 16:13:07 +0900

    BookAPI: search Book by ISBN

--- src/main/java/api/BookAPI.java
+++ src/main/java/api/BookAPI.java
@@ -8,12 +8,44 @@ import model.*;
 
 public class BookAPI {
     public static Book selectBookByISBN(String isbn) throws SQLException {
-        Book book = new Book();
-
+        Book b = new Book();
+        
+        b.authors = new ArrayList<String>();
         // Reference: Documentation 5.2 - 1.a
         // TODO
 
-        return book;
+        Database db = new Database();
+        PreparedStatement pstatement = db.connection().prepareStatement("SELECT * FROM Book WHERE isbn=?");    
+        pstatement.setString(1, isbn);    
+        ResultSet rs = null;
+        rs = pstatement.executeQuery();
+
+        if (rs.next()) {
+            System.out.println(rs.getString("title"));
+            b.isbn = rs.getString("isbn");
+            b.title = rs.getString("title");
+            b.price = rs.getInt("unit_price");
+            b.availableCopies = rs.getInt("no_copies");
+            System.out.println(b.isbn);
+            System.out.println(b.title);
+            System.out.println(b.price);
+            System.out.println(b.availableCopies);
+        } 
+        
+        PreparedStatement pstatement2 = db.connection().prepareStatement("SELECT name FROM Author Where isbn = ?");    
+        pstatement2.setString(1, isbn);    
+        ResultSet rs2 = null;
+        rs2 = pstatement2.executeQuery();
+
+        while (rs2.next()){
+            String authorsstring = rs2.getString("name");
+            b.authors.add(authorsstring);
+            System.out.println(authorsstring);
+        }
+                
+        db.close();
+
+        return b;
     }
 
     public static List<Book> selectBooksByTitle(String title) throws SQLException {
